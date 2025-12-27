34f55b2af757c4100ffc26ccf3700f5af4076a06
SaintA01 <exceladebayo112@gmail.com>
2025-12-27 10:27:24 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
-import VideoGenerator from './components/VideoGenerator'
-import AdminPanel from './components/AdminPanel'
+import VideoGenerator from '../components/VideoGenerator'
+import AdminPanel from '../components/AdminPanel'
 
 export default function Home() {
   const [isAdmin, setIsAdmin] = useState(false)
